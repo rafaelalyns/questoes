e7778fbbe5eafe7f9c90a0087becf9804d5ef403
rafaelalyns <127249548+rafaelalyns@users.noreply.github.com>
2023-03-07 19:00:41 -0300

    Add files via upload

---
+++ primeiraQuestao.java
@@ -0,0 +1,27 @@
+package questoes1;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class primeiraQuestao {
+
+	public static void main(String[] args) {
+		int idade = 22;
+		System.out.println(idade);
+
+		if (idade >= 18) {
+			String maior = "É maior de idade.";
+			System.out.println(maior);	
+		} else {
+			String menor = "É menor de idade";
+			System.out.println(menor);
+		}
+		
+	
+		
+		}
+
+	}
