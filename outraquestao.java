7ea2bbe697dfa1575e77724110bd0e25c4ecc908
rafaelalyns <127249548+rafaelalyns@users.noreply.github.com>
2023-03-08 12:28:29 -0300

    outra questao atualizada

--- outraquestao.java
+++ outraquestao.java
@@ -7,33 +7,34 @@ public class outraquestao {
     public static void main(String[] args) {
       try (Scanner operacao = new
         Scanner(System.in)) {
+          Scanner primeiro = new       
+                  Scanner(System.in);
+              System.out.println("Digite o primeiro número: ");
+              int primeiroNumero = primeiro.nextInt();
+              
+              Scanner segundo = new       
+                      Scanner(System.in);
+                  System.out.println("Digite o segundo número: ");
+                  int segundoNumero = segundo.nextInt();
+              
         System.out.println("Escreva o símbolo da operação: ");
+        
                  String operacao1 = operacao.nextLine(); {
                 switch (operacao1) {
                 case "+":
-                System.out.println("A operacao é adicao");
+                System.out.println(primeiroNumero + segundoNumero);
                 break;
                 case "-":
-                    System.out.println("A operacao é subtracao");
+                    System.out.println(primeiroNumero - segundoNumero);
                     break;
                 case "*":
-                    System.out.println("A operacao é multiplicacao");
+                    System.out.println(primeiroNumero * segundoNumero);
                     break;
                 case "/":
-                    System.out.println("A operacao é divisao");
+                    System.out.println(primeiroNumero / segundoNumero);
                     break;
                 }
-                Scanner primeiro = new       
-                Scanner(System.in);
-            System.out.println("Digite o primeiro número: ");
-            int primeiroNumero = primeiro.nextInt();
-            
-            Scanner segundo = new       
-                    Scanner(System.in);
-                System.out.println("Digite o segundo número: ");
-                int segundoNumero = segundo.nextInt();
-            
-            
+                
                     
         }
 
