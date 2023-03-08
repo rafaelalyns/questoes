c789172b5db51fe0fe194d8a73cd918b71616185
rafaelalyns <127249548+rafaelalyns@users.noreply.github.com>
2023-03-08 14:50:45 -0300

    primeira questao

---
+++ questoes.java
@@ -0,0 +1,17 @@
+package estruturaRepeticao;
+
+public class questoes {
+
+    public static void main(String[] args) {
+        int num = 2;
+     for (int multi = 0; multi <= 10; multi++) {
+       System.out.println(num+"*"+multi+"="+num * multi);
+         
+     }
+     
+     }
+      
+              
+                    
+        
+    }
